d0c7bca03b9bc50aef4b2b9bcba507617ba76275
Mark R Baker <mrbaker1917@gmail.com>
2021-07-18 17:50:18 -0700

    basic structure of form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { FaForumbee } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Spelling Bee Helper! All you need to be a Genius!
-        </p>
       </header>
+      <form>
+        <h1>Welcome to Spelling Bee Helper!</h1>
+        <a
+          rel="noreferrer"
+          target="_blank"
+          className="link"
+          href="https://www.nytimes.com/puzzles/spelling-bee"
+          title="Click to go to the puzzle!"
+        >{`{ nytimes spelling bee }`}</a>
+        <h3>All you need to become a Genius!</h3>
+        <FaForumbee className="forum-bee" />
+        <label className="letters-label">
+          Enter the letters around the center of the honeycomb here:
+        </label>
+        <input name="letters" className="letters-input"></input>
+        <label className="center-label">Enter the center letter here:</label>
+        <input name="center" className="center-input"></input>
+        <p>Press the submit button!</p>
+        <button className="btn" type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
