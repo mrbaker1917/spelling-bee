b50eba96eed89f16fa6ce5e8a7bc383cff400f67
Mark R Baker <mrbaker1917@gmail.com>
2021-07-20 21:28:52 -0700

    got words to render to page!

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -3,25 +3,27 @@ import getWords from "../utils/api";
 
 export default class Form extends React.Component {
   constructor(props) {
-    super(props) 
+    super(props);
     this.state = {
       letters: "",
       center: "",
       clicked: false,
+      c_words: [],
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-
   handleSubmit(event) {
     event.preventDefault();
-    const {letters, center} = this.state;
+    const { letters, center } = this.state;
     const re = new RegExp(`^([${letters}])*${center}+([${letters}])*$`);
     getWords().then((words) => {
-    let c_words = words.filter(word => word.match(re) !== null);
-    console.log(c_words)
-    })
+      this.setState({
+        clicked: true,
+        c_words: Object.values(words).filter((word) => word.match(re) !== null),
+      });
+    });
   }
 
   handleInputChange(event) {
@@ -29,53 +31,52 @@ export default class Form extends React.Component {
     const value = target.value;
     const name = target.name;
     this.setState({
-      [name]: value
-    })
+      [name]: value,
+    });
   }
 
   render() {
     if (this.state.clicked === true) {
       return (
-        <div>{this.c_words}</div>
-      )
+        <ul>
+          {this.state.c_words.map((word) => {
+            return <li key={word}>{word}</li>;
+          })}
+        </ul>
+      );
     }
     return (
-    <React.Fragment>
-      <form className="form">
-      <label htmlFor="letters" className="letters">
-        Enter letters here:
-      </label>
-        <input
-          type="text"
-          name="letters"
-          className="letters"
-          placeholder="letters"
-          autoComplete="off"
-          value={this.state.letters}
-          onChange={this.handleInputChange}
-        />
-        <label htmlFor="center" className="center">
-        Enter center letter here:
-      </label>
-        <input
-          type="text"
-          name="center"
-          className="center"
-          placeholder="center letter"
-          autoComplete="off"
-          value={this.state.center}
-          onChange={this.handleInputChange}
-        />
-        <button
-          className="btn"
-          type="submit"
-          value="Submit"
-          onClick={() => this.setState({clicked: true})}
-        >
-          SUBMIT
-        </button>
-    </form>
-    </React.Fragment>
-    )}
-
+      <React.Fragment>
+        <form className="form" onSubmit={this.handleSubmit}>
+          <label htmlFor="letters" className="letters">
+            Enter letters here:
+          </label>
+          <input
+            type="text"
+            name="letters"
+            className="letters"
+            placeholder="letters"
+            autoComplete="off"
+            value={this.state.letters}
+            onChange={this.handleInputChange}
+          />
+          <label htmlFor="center" className="center">
+            Enter center letter here:
+          </label>
+          <input
+            type="text"
+            name="center"
+            className="center"
+            placeholder="center letter"
+            autoComplete="off"
+            value={this.state.center}
+            onChange={this.handleInputChange}
+          />
+          <button className="btn" type="submit" value="Submit">
+            SUBMIT
+          </button>
+        </form>
+      </React.Fragment>
+    );
+  }
 }
