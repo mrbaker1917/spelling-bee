5990471efb504c7e20f624f2b7f67e517fb0c303
Mark R Baker <mrbaker1917@gmail.com>
2021-10-16 14:57:25 -0700

    changes to word list

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,8 @@ const fetch = require("node-fetch");
 
 export default function getWords() {
   return fetch(
-    `https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt`
+    // `https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt`
+    `./refined_list.txt`
   )
     .then((res) => res.text())
     .then((data) => {
