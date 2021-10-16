4c31a9d993c55b897bd41fae40b475c19b4c8e2e
Mark R Baker <mrbaker1917@gmail.com>
2021-10-16 11:28:31 -0700

    alters text file link

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 export default function getWords() {
   return fetch(
     // `https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt`
-    `https://gist.github.com/h3xx/1976236#file-wiki-100k-txt`
+    `https://gist.github.com/h3xx/1976236`
   )
     .then((res) => res.text())
     .then((data) => {
