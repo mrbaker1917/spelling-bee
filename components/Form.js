e69b6775bc361be36bb3bb1dcb3dd97fad28f2f6
mrbaker1917 <mrbaker1917@gmail.com>
2026-05-18 12:27:38 -0700

    puts reset button always visible.

--- components/Form.js
+++ components/Form.js
@@ -102,6 +102,9 @@ export default class Form extends React.Component {
     if (this.state.clicked === true) {
       return (
         <React.Fragment>
+          <button className="btn btn-reset-floating" onClick={this.refreshPage}>
+            Reset
+          </button>
           <div className="pangrams">
             <h2>Possible Pangrams:</h2>
             <ul>
@@ -110,9 +113,6 @@ export default class Form extends React.Component {
               })}
             </ul>
           </div>
-          <button className="btn" onClick={this.refreshPage}>
-            Reset
-          </button>
           <div className="all-words">
             <h2>All Possible ({this.state.c_words.length}) Words:</h2>
             <ul>
@@ -121,9 +121,6 @@ export default class Form extends React.Component {
               })}
             </ul>
           </div>
-          <button className="btn" onClick={this.refreshPage}>
-            Reset
-          </button>
         </React.Fragment>
       );
     }
